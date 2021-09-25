2eaa3045a7aec01912f4c461201c46aa8c12b6d9
vishnu0921 <ba11@iitbbs.ac.in>
2021-09-25 21:03:27 +0530

    Added router

--- src/app.js
+++ src/app.js
@@ -4,9 +4,8 @@ const app = express();
 const hbs = require('hbs');
 require('./db/conn');
 const User = require('./models/user');
-const bcrypt = require('bcryptjs');
 const cookieParser = require('cookie-parser');
-const auth = require("./auth");
+const registerRouter = require('./routers/register');
 
 const port = process.env.PORT || 8000;
 
@@ -21,80 +20,7 @@ app.use(cookieParser());
 app.set('view engine', 'hbs');
 hbs.registerPartials(partialsPath);
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-app.get('/login', auth, (req,res) => {
-    res.render('login');
-});
-
-app.get('/signup', (req, res) => {
-    res.render('signup');
-});
-
-app.post('/login', async (req, res) => {
-    try {
-        const email = req.body.email;
-        const password = req.body.password;
-        const loginUser = await User.findOne({ email: email});
-        
-        if(!loginUser){
-            res.status(400).send("<h1> Invalid Email </h1>")
-            return;
-        }
-
-        const checkPassword = await bcrypt.compare(password, loginUser.password);
-
-        
-        const token = await loginUser.generateToken();
-
-        res.cookie("jwt", token, {
-            expires: new Date(Date.now() + 500000),
-            httpOnly: true
-        });
-
-        if(checkPassword == true){
-            res.render("welcome", {name: loginUser.name});
-        } else{
-            res.status(400).send("<h1> Invalid Password </h1>")
-        }
-
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-
-app.post('/signup', async (req,res) => {
-    try {
-        const password = req.body.password;
-        const confirmpassword = req.body.confirmpassword;
-
-        if(password === confirmpassword){
-            const newUser = new User({
-                name: req.body.name,
-                email: req.body.email,
-                phone: req.body.phone,
-                gender: req.body.gender,
-                password: req.body.password
-            });
-
-            const token = await newUser.generateToken();
-
-            res.cookie("jwt", token, {
-                expires: new Date(Date.now() + 600000),
-                httpOnly: true
-            });
-
-            const addResult = await newUser.save();
-            res.send(addResult);
-
-        }
-        
-    } catch (err) {
-        res.status(400).send(err);
-    }
-})
+app.use(registerRouter);
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
---
+++ src/routers/register.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const User = require('../models/user');
+const auth = require("../auth");
+const bcrypt = require('bcryptjs');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+router.get('/login', auth, (req,res) => {
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        const loginUser = await User.findOne({ email: email});
+        
+        if(!loginUser){
+            res.status(400).send("<h1> Invalid Email </h1>")
+            return;
+        }
+
+        const checkPassword = await bcrypt.compare(password, loginUser.password);
+
+        
+        const token = await loginUser.generateToken();
+
+        res.cookie("jwt", token, {
+            expires: new Date(Date.now() + 500000),
+            httpOnly: true
+        });
+
+        if(checkPassword == true){
+            res.render("welcome", {name: loginUser.name});
+        } else{
+            res.status(400).send("<h1> Invalid Password </h1>")
+        }
+
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+router.post('/signup', async (req,res) => {
+    try {
+        const password = req.body.password;
+        const confirmpassword = req.body.confirmpassword;
+
+        if(password === confirmpassword){
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                phone: req.body.phone,
+                gender: req.body.gender,
+                password: req.body.password
+            });
+
+            const token = await newUser.generateToken();
+
+            res.cookie("jwt", token, {
+                expires: new Date(Date.now() + 600000),
+                httpOnly: true
+            });
+
+            const addResult = await newUser.save();
+            res.render('welcome', {name: addResult.name});
+        }
+        
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+
+router.get('/logout', (req, res) => {
+    try {
+        res.clearCookie("jwt");
+        res.render("index");
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+module.exports = router;
