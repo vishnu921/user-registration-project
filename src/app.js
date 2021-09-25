0d5f3e29f074f35f7784f2aa288de71b859cf94c
vishnu0921 <ba11@iitbbs.ac.in>
2021-09-25 20:28:42 +0530

    Added views using hbs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const hbs = require('hbs');
 
 const port = process.env.PORT || 8000;
 
-app.get('/', (req,res) => {
-    res.send("Hello World");
+const partialsPath = path.join(__dirname, "../views/partials");
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(partialsPath);
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/login', (req,res) => {
+    res.render('login');
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup');
 });
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
-})
+});
